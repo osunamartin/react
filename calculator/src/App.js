08f1f8d730b730e0c46acc733ce1ebc9a1b5fb21
osunamartin <martin.osuna13@gmail.com>
2023-02-02 11:27:02 -0300

    finished

---
+++ calculator/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
+
+function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert ("Agregue un valor válido")
+    }
+    
+  };
+
+  return (
+    <div className='App'>
+      {/* <div className="freecodecamp-logo-contenedor">
+        <img 
+        src={freeCodeCampLogo} 
+        alt="logo de freecodecamp" 
+        className='freecodecamp-logo' />
+      </div> */}
+      <div className="contenedor-calculadora">
+        <Pantalla input={input} />
+        <div className="fila">
+          <Boton manejarClick={agregarInput}>1</Boton>
+          <Boton manejarClick={agregarInput}>2</Boton>
+          <Boton manejarClick={agregarInput}>3</Boton>
+          <Boton manejarClick={agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClick={agregarInput}>4</Boton>
+          <Boton manejarClick={agregarInput}>5</Boton>
+          <Boton manejarClick={agregarInput}>6</Boton>
+          <Boton manejarClick={agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClick={agregarInput}>7</Boton>
+          <Boton manejarClick={agregarInput}>8</Boton>
+          <Boton manejarClick={agregarInput}>9</Boton>
+          <Boton manejarClick={agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClick={calcularResultado}>=</Boton>
+          <Boton manejarClick={agregarInput}>0</Boton>
+          <Boton manejarClick={agregarInput}>.</Boton>
+          <Boton manejarClick={agregarInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ clickCounter/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/fcclogo.jpg'
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import { useState } from 'react';
+
+function App() {  
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClick = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  };
+  return (
+    <div className='App'>
+      {/* <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='logo de freecodecamp'/>
+
+
+      </div> */}
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClicks}/>
+        <Boton
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick}/>
+          <Boton
+            texto='Reiniciar'
+            esBotonDeClick={false}
+            manejarClick={reiniciarContador} />
+
+      </div>
+    </div>
+      
+  );
+}
+
+export default App;
